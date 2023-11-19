97df63b78be5e9c62be6ba2ca8bd6a6f34b56050
ThuanHandsomeee <thuanb2014793@student.ctu.edu.vn>
2023-11-19 15:15:38 +0700

    commit16

--- public/shopnow.php
+++ public/shopnow.php
@@ -61,16 +61,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         echo $idproduct
                             ?>">
                         <div class="col-md-6">
-                            <label for="name">Your Name</label>
+                            <label>Your Name</label>
                             <input type="text" class="form-control" name="namecus" placeholder="Your Name" required>
                         </div>
                         <div class="col-md-6">
-                            <label for="phone number">Phone Number</label>
+                            <label>Phone Number</label>
                             <input type="phone number" class="form-control" name="phone" placeholder="Phone Number"
                                 required>
                         </div>
                         <div class="col-12">
-                            <label for="address">Address</label>
+                            <label>Address</label>
                             <input type="Address" class="form-control" name="address" placeholder="Address" required>
                         </div>
                         <div>
